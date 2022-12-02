aa1e685e54c7eeec332b1e709b7458c8b9c45a42
kwakhyun <khyun2348@naver.com>
2022-12-02 21:27:42 +0900

    Modify: tailwind config content path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./src/pages/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
+  content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
