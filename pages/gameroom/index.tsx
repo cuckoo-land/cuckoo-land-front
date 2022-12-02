057d9fb7ff8f9d3ed94c23fad154e8aae906f05e
JUNGJIN <ojj824@naver.com>
2022-12-02 16:41:13 +0900

    Design: gameroom desgin set #1

--- pages/gameroom/index.tsx
+++ pages/gameroom/index.tsx
@@ -1,12 +1,17 @@
-import Table from '@components/gameroom/table';
+import Member from '@components/gameroom/member';
 import Layout from '@components/layout';
+import Button from '@components/button';
+import Chat from '@components/gameroom/chat';
 
 function GameRoom() {
   return (
     <Layout title="Gameroom" seoTitle="Gameroom">
-      <div>
-        <Table />
+      <div className="h-20 font-bold text-center">header</div>
+      <Member />
+      <div className="flex">
+        <Button text="hi" />
       </div>
+      <Chat />
     </Layout>
   );
 }
