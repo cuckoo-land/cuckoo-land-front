18ea43dc2c8c470be022166b5d694c1c933afcfc
kwakhyun <khyun2348@naver.com>
2022-12-02 21:40:20 +0900

    Feat: accessToken check

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,18 @@ import type { AppProps } from 'next/app';
 import '@styles/globals.css';
 import GlobalStyle from '@styles/GlobalStyles';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const { pathname } = router;
   const isStartPage = pathname === '/login' || pathname === '/join';
+
+  useEffect(() => {
+    if (!localStorage.getItem('accessToken')) {
+      router.push('/login');
+    }
+  }, []);
 
   return (
     <>
