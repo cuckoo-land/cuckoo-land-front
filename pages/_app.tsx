01230048b1afcd928a6ea7d8cd9d92a33ddaf997
kwakhyun <khyun2348@naver.com>
2022-12-02 21:28:46 +0900

    Feat: start page background music

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
 import type { AppProps } from 'next/app';
 import '@styles/globals.css';
 import GlobalStyle from '@styles/GlobalStyles';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const { pathname } = router;
+  const isStartPage = pathname === '/login' || pathname === '/join';
+
   return (
     <>
+      {isStartPage && (
+        <audio autoPlay loop src="/intro-bgm.mp3">
+          <track default kind="captions" />
+        </audio>
+      )}
       <GlobalStyle />
       <Component {...pageProps} />
     </>
