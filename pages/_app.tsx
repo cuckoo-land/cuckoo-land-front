e29b9661ecaec22cd030129b309d7146d6f36464
momoco-git <gnspope@naver.com>
2022-12-06 23:03:05 +0900

    feat:menu

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import '@styles/globals.css';
 import GlobalStyle from '@styles/GlobalStyles';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import Menubar from '@components/menubar/Menubar';
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -11,20 +10,19 @@ export default function App({ Component, pageProps }: AppProps) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const isStartPage = pathname === '/login' || pathname === '/join';
 
-  useEffect(() => {
-    if (!localStorage.getItem('accessToken')) {
-      router.push('/login');
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (!localStorage.getItem('accessToken')) {
+  //     router.push('/login');
+  //   }
+  // }, []);
 
   return (
     <>
-      <Menubar />
-      {isStartPage && (
+      {/* {isStartPage && (
         <audio autoPlay loop src="/intro-bgm.mp3">
           <track default kind="captions" />
         </audio>
-      )}
+      )} */}
       <GlobalStyle />
       <Component {...pageProps} />
     </>
