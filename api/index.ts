772189c1f42cd8d3fea68f7cd2ac3ae2b24fe4ef
kwakhyun <khyun2348@naver.com>
2022-12-11 19:36:25 +0900

    Feat: Add rankingAPI

--- api/index.ts
+++ api/index.ts
@@ -4,13 +4,22 @@ export const authAPI = {
   join: (data: any) => api.post('/join', data),
   memberidCheck: (data: any) => api.post('/join/idcheck', data),
   nicknameCheck: (data: any) => api.post('/join/nickcheck', data),
+
   login: (data: any) => api.post('/login', data),
   guestLogin: (data: any) => api.post('/guest/login', data),
   guestLogout: () => api.delete('/guest/out'),
 };
 
-
 export const MenuAPI = {
   getUserInfo: () => api.post(``),
 };
+
 export const gameroomAPI = {};
+
+export const rankingAPI = {
+  getMafiaRankList: () => api.get(`/ranking/mafia/total`),
+  getMafiaRankDetail: (id: number) => api.get(`/ranking/mafia/${id}`),
+
+  getMajorityRankList: () => api.get(`/ranking/majority/total`),
+  getMajorityRankDetail: (id: number) => api.get(`/ranking/majority/${id}`),
+};
