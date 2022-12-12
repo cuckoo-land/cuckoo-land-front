b6ec550f6d0b824d4aba293995478a5752b44621
kwakhyun <khyun2348@naver.com>
2022-12-12 15:54:16 +0900

    Feat:Redirect to root path login page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,26 +2,26 @@ import type { AppProps } from 'next/app';
 import '@styles/globals.css';
 import GlobalStyle from '@styles/GlobalStyles';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const { pathname } = router;
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const isStartPage = pathname === '/login' || pathname === '/join';
 
-  // useEffect(() => {
-  //   if (!localStorage.getItem('accessToken')) {
-  //     router.push('/login');
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (!localStorage.getItem('accessToken')) {
+      router.push('/login');
+    }
+  }, []);
 
   return (
     <>
-      {/* {isStartPage && (
-        <audio autoPlay loop src="/intro-bgm.mp3">
+      {isStartPage && (
+        <audio autoPlay loop src="">
           <track default kind="captions" />
         </audio>
-      )} */}
+      )}
       <GlobalStyle />
       <Component {...pageProps} />
     </>
--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="bg-black font-mono min-h-screen">
-      <h1 className="text-white">$ Hello Next.js</h1>
-    </div>
-  );
-}
