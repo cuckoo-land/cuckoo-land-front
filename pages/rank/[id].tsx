8d593259c68f5d075da995e9b8cbe7e150888666
kwakhyun <khyun2348@naver.com>
2022-12-13 23:55:03 +0900

    Feat: Add ranking detail page

--- pages/rank/[id].tsx
+++
@@ -1,9 +0,0 @@
-export default function MafiaDetail() {
-  return (
-    <div
-      className="w-full flex flex-col max-h-[100vh] pb-16
-             justify-start space-y-3 overflow-y-scroll mt-8 scrollbar-hide">
-      <h1>Mafia Game Ranking Detail</h1>
-    </div>
-  );
-}
